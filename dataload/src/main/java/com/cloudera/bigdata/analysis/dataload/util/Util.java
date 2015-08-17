af6485aef88fe052fa7a775cc668261f1b3a756e
centiteo <dery.chen@gmail.com>
2015-08-18 00:07:23 +0800

    add create table logic

--- dataload/src/main/java/com/cloudera/bigdata/analysis/dataload/util/Util.java
+++ dataload/src/main/java/com/cloudera/bigdata/analysis/dataload/util/Util.java
@@ -278,6 +278,84 @@ public class Util {
     return splitKeys;
   }
 
+  public static byte[][] genSplitKeysAlphaDig(String prefixesStr, int splitSize)
+      throws Exception {
+    if (prefixesStr == null) {
+      prefixesStr = "";
+    }
+    String[] prefixes = prefixesStr.split(",");
+    int round = 1;
+    int num = 0;
+    if (!checkSplitSizeAlphaDig(splitSize)) {
+      throw new TableDisabledException(
+          "The split size should be a number with 62^n.");
+    }
+    if (prefixesStr.equals("") || prefixes.length == 0) {
+      prefixes = new String[62];
+      for (int i = 0; i < 10; i++) {
+        prefixes[i] = new String(new char[] { (char) ('0' + i) });
+      }
+      for (int i = 10; i < 36; i++) {
+        prefixes[i] = new String(new char[] { (char) ('A' + (i - 10)) });
+      }
+      for (int i = 36; i < 62; i++) {
+        prefixes[i] = new String(new char[] { (char) ('a' + (i - 36)) });
+      }
+    }
+    prefixes = trimStrings(prefixes);
+
+    while (round < splitSize) {
+      num++;
+      round *= 62;
+    }
+
+    byte[][] splitKeys = new byte[splitSize * prefixes.length][];
+
+    char[] alphadigit = new char[num];
+    String[] suffixes = new String[splitSize];
+    for (int i = 0; i < splitSize; i++) {
+      suffixes[i] = "";
+    }
+    if (num > 0) {
+      for (int i = 0; i < splitSize; i++) {
+        int k = num - 1;
+        int j = i;
+        while (j != 0) {
+          alphadigit[k--] = mapChar(j % 62);
+          j /= 62;
+        }
+        for (int m = 0; m <= k; m++) {
+          alphadigit[m] = '0';
+        }
+        
+        String suffix = new String(alphadigit);
+        suffixes[i] = suffix;
+      }
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < prefixes.length; i++) {
+      for (int j = 0; j < suffixes.length; j++) {
+        splitKeys[i * suffixes.length + j] =
+            Bytes.toBytes(prefixes[i] + suffixes[j]);
+        sb.append(prefixes[i] + suffixes[j] + "|");
+      }
+    }
+    LOG.info("SplitKey: " + sb);
+    System.out.println("SplitKey: " + sb);
+    return splitKeys;
+  }
+
+  private static char mapChar(int val) {
+    if (val <= 9) {
+      return (char) ('0' + val);
+    } else if (val >= 10 && val <= 35) {
+      return (char) ('A' + val - 10);
+    } else {
+      return (char) ('a' + val - 36);
+    }
+  }
+
   private static String[] trimStrings(String[] orignal) {
     String[] result = new String[orignal.length];
 
@@ -305,6 +383,27 @@ public class Util {
           return false;
         }
         splitSize /= 10;
+      }
+    }
+  }
+
+  private static boolean checkSplitSizeAlphaDig(int splitSize) {
+    if (splitSize < 1) {
+      return false;
+    }
+
+    while (true) {
+      if (splitSize < 62) {
+        if (splitSize == 1) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        if (splitSize % 62 != 0) {
+          return false;
+        }
+        splitSize /= 62;
       }
     }
   }
---
+++ server/src/main/java/com/cloudera/bigdata/analysis/hbase/mapreduce/CreateTable.java
@@ -0,0 +1,63 @@
+package com.cloudera.bigdata.analysis.hbase.mapreduce;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.io.compress.Compression.Algorithm;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import com.cloudera.bigdata.analysis.dataload.util.Util;
+
+/**
+ * Assume the leading RowKey only contains Alphabet and Digit
+ * 
+ * @author centiteo
+ *
+ */
+public class CreateTable {
+
+  /**
+   * <tableName> <splitSize> [<splitPrefix>]
+   * 
+   * @param args
+   * @throws Exception
+   */
+  public static void main(String[] args) throws Exception {
+    System.out.println(Util.genSplitKeysAlphaDig("", 62));
+
+    if (args.length < 2) {
+      System.out.println("Usage: "
+          + "com.cloudera.bigdata.analysis.hbase.mapreduce.CreateTable"
+          + " <tableName>" + " <splitSize>"
+          + " [<splitPrefix>]   #comma separated string list");
+    }
+
+    String tableName = args[0];
+    int splitSize = Integer.parseInt(args[1]);
+    String splitPrefix = args[2];
+
+    createTableInner(tableName, splitSize, splitPrefix);
+  }
+
+  public static void createTableInner(String tableName, int splitSize,
+      String splitPrefix) throws Exception {
+    Configuration conf = HBaseConfiguration.create();
+    HBaseAdmin hAdmin = new HBaseAdmin(conf);
+
+    HColumnDescriptor columnDescriptor =
+        new HColumnDescriptor(Bytes.toBytes("f"));
+    columnDescriptor.setCompressionType(Algorithm.SNAPPY);
+    columnDescriptor.setMaxVersions(3);
+
+    HTableDescriptor descriptor =
+        new HTableDescriptor(TableName.valueOf(tableName));
+    descriptor.addFamily(columnDescriptor);
+    hAdmin.createTable(descriptor, Util.genSplitKeys(splitPrefix, splitSize));
+
+    hAdmin.close();
+  }
+
+}
